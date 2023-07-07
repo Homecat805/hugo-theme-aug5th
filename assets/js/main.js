7fa9a5cc6890173ace44f4be9a494aaf9056bc83
homecat805 <13050082@qq.com>
2023-07-07 13:30:03 +0800

    Add menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,9 @@ $(document).ready(function(){
     const wOpenBtn = $('#w-screen').children('.open');
     const wCloseBtn = $('#w-screen').children('.close');
 
+    const menuItemName = $('.menu').find('.item-name');
+    const menuItemToggle = $('.menu').find('.item-toggle');
+
     nOpenBtn.click(function(){
         sidebar.animate({left:'0'});
         nCloseBtn.show();
@@ -26,12 +29,16 @@ $(document).ready(function(){
         sidebar.animate({width:'60px'});
         wCloseBtn.show();
         wOpenBtn.hide();
+        menuItemName.hide(250);
+        menuItemToggle.hide(250);
     });
 
     wCloseBtn.click(function(){
         sidebar.animate({width:'250px'});
         wOpenBtn.show();
         wCloseBtn.hide();
+        menuItemName.show();
+        menuItemToggle.show();
     });
 
 });
