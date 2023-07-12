8f87343c113fd9f963d22a119da18e4a256d7ac5
homecat805 <13050082@qq.com>
2023-07-12 13:45:40 +0800

    Modify baseof

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,52 +6,28 @@ $(document).ready(function(){
 
     'use strict';
 
-    // Sidebar
+    // Sider Controll Start
 
-    const sidebar = $('#sidebar');
-    const nOpenBtn = $('#n-screen').children('.open');
-    const nCloseBtn = $('#n-screen').children('.close');
-    const wOpenBtn = $('#w-screen').children('.open');
-    const wCloseBtn = $('#w-screen').children('.close');
+    const sider = $('#sider');
+    const siderOpenBtn = $('#sider-button').children('.open');
+    const siderCloseBtn = $('#sider-button').children('.close');
 
-    const menuItemName = $('.menu').find('.item-name');
-    const menuItemToggle = $('.menu').find('.item-toggle');
-
-
-    nOpenBtn.click(function(){
-        sidebar.animate({left:'0'});
-        nCloseBtn.show();
-        nOpenBtn.hide();
+    siderOpenBtn.click(function(){
+        sider.animate({left:'0'});
+        siderCloseBtn.show();
+        siderOpenBtn.hide();
     });
 
-    nCloseBtn.click(function(){
-        sidebar.animate({left:'-270px'});
-        nOpenBtn.show();
-        nCloseBtn.hide();
+    siderCloseBtn.click(function(){
+        sider.animate({left:'-270px'});
+        siderOpenBtn.show();
+        siderCloseBtn.hide();
     });
 
-    wOpenBtn.click(function(){
-        sidebar.animate({width:'60px'});
-
-        wCloseBtn.show();
-        wOpenBtn.hide();
-        menuItemName.hide(250);
-        menuItemToggle.hide(250);
-
-    });
-
-    wCloseBtn.click(function(){
-        sidebar.animate({width:'250px'});
-        wOpenBtn.show();
-        wCloseBtn.hide();
-        menuItemName.show();
-        menuItemToggle.show();
-    });
+    // Sider Controll End
 
     // Menu
-
-
-  
+ 
     menuItemToggle.click(function(){
 
         $(this).parents('.menu-item').siblings().children('.sub-menu').slideUp();
